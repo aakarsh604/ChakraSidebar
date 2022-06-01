cdf4004aee25d7145bf2516afd91bfa4e6b34907
aakarsh604 <aakarsh604@gmail.com>
2022-06-01 16:57:50 +0530

    ook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Sidebar2 from './Components/Sidebar2.tsx';
+
+function App() {
+  return (
+    <div className="App">
+        <Sidebar2 />
+    </div>
+  );
+}
+
+export default App;
